b9ba0a115dccd910bff6be4ad5c43e67e698a461
MustafaAlsadah <sadah9999@gmail.com>
2022-05-22 23:02:21 +0300

    designing the components

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,15 +1,19 @@
 import React from "react";
+import reactMark from "../images/reactjs-icon 2.svg"; 
 
 export default function MainContent() {
     return (
-        <div>
+        <div className="main-content-container">
             <h1>Fun Facts About React</h1>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
+            <ul className="main--facts-list">
+                <div>
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100K stars on GitHub</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </div>
+                <img className="main--react-mark" src={reactMark} alt="react-mark"/>
             </ul>
         </div>
     );
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,13 +3,13 @@ import ReactLogo from "../images/React-icon.png"
 
 export default function Navbar(){
     return(
-        <nav>
-            <div>
-                <img src={ReactLogo} alt="React-icon" width="10%" />
-                <h1>ReactFacts</h1>
+        <nav className="navbar">
+            <div className="logo-side">
+                <img src={ReactLogo} alt="React-icon"/>
+                <h1 className="logo-txt">ReactFacts</h1>
             </div>
-            <div>
-                <h2>React Course-Project 1</h2>
+            <div className="annotation-side">
+                <h4 className="annotation-txt">React Course-Project 1</h4>
             </div>
         </nav>
     )
